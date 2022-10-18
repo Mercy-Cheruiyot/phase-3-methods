e98e8547c27222d30be800bd424e92ed2b24fbc4
Mercy <mercycheruiyot2013@gmail.com>
2022-10-18 06:43:43 +0300

    Completed Assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,31 @@
 # Your code here!
+def  greet_programmer
+  puts  "Hello, programmer!" 
+end
+greet_programmer
+
+ def greet(name="Naureen")
+    puts "Hello, #{name}!"
+ end
+
+ def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+ end
+ greet_with_default("Sunny")
+
+ def add(num1, num2)
+    return num1 + num2
+  end
+
+ sum = add(1, 2)
+ sum
+  
+ def halve (number)
+    if number.class != Integer
+
+        return nil
+      end
+       number/2
+    end
+     halve(6)
+    
